c0fa01bab54543243bba9d83142bda5ae6aaa7d5
Volodymyr Osmak <darkxahtep@gmail.com>
2015-07-29 02:09:43 +0300

    Added file for scrapper tests

---
+++ test/scrapper.js
@@ -0,0 +1,9 @@
+var proxyquire = require('proxyquire');
+var chai = require('chai');
+var expect = chai.expect;
+chai.use(require('sinon-chai'));
+require('mocha-sinon');
+
+describe('Scrapper', function() {
+
+})
