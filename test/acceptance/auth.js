f12dd91bcfe52d29ac283e47147438b497c3fe5b
Volodymyr Osmak <darkxahtep@gmail.com>
2015-07-30 03:55:48 +0300

    removed not needed dependencies for acceptance tests

--- test/acceptance/auth.js
+++ test/acceptance/auth.js
@@ -1,19 +1,15 @@
-var domain = require("domain");
-var proxyquire = require('proxyquire');
 var chai = require('chai');
 var expect = chai.expect;
-chai.use(require('sinon-chai'));
-require('mocha-sinon');
 var vkAuth = require('../../');
 
 describe('vkAuth', function() {
+    this.timeout(7000);
+
     beforeEach(function() {
         this.auth = vkAuth(process.env.VK_CLIENT_ID, 'audio');
     })
 
     it('should successfully get token', function(done) {
-        this.timeout(5000);
-
         this.auth.authorize(process.env.VK_USERNAME, process.env.VK_PASSWORD, function(err, token) {
             expect(err).to.be.null;
             expect(token.access_token).to.be.a('string');
@@ -22,8 +18,6 @@ describe('vkAuth', function() {
     })
 
     it('should return error about invalid credentials', function(done) {
-        this.timeout(5000);
-
         this.auth.authorize('user@example.com', 'password', function(err, token) {
             expect(token).to.be.undefined;
             expect(err.message).to.equal('Incorrect login or password\n');
